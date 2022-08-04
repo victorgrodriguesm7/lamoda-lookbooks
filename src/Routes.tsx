1f0a567aea3fb70d61720e83028384383e144808
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-04 16:49:27 -0300

    feat: add private admin pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,13 +3,21 @@ import {
     Routes as Switch,
     Route,
 } from "react-router-dom";
+import { PrivateRoute } from "./components/Admin";
+import AdminHome from "./pages/AdminHome";
+import AdminLogin from "./pages/AdminLogin";
 import Home from "./pages/Home";
+
 
 export default function Routes() {
   return (
     <BrowserRouter>
         <Switch>
             <Route path="/" element={<Home/>}/>
+            <Route path="/admin" element={<AdminLogin/>}/>
+            <Route element={<PrivateRoute/>}>
+              <Route path="/admin/home" element={<AdminHome/>}/>
+            </Route>
         </Switch>
     </BrowserRouter>
   )
