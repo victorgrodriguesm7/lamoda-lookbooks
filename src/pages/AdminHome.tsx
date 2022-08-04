1c221014c5297b62b6eb0d872605757e23278d4f
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-04 16:49:09 -0300

    feat: create Dummy HomePage

---
+++ src/pages/AdminHome.tsx
@@ -0,0 +1,9 @@
+import { AdminHome } from "../components/Admin";
+
+export default function AdminHomePage() {
+  return (
+    <main style={{ height: "100vh" }}>
+        <AdminHome/>
+    </main>
+  )
+}
