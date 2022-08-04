f9e5bf29566f678a74552801e5024be52ab085b9
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-04 16:48:33 -0300

    feat: add AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import { AuthProvider } from './context/AuthContext';
 import { GlobalStyle, Theme } from './global';
 
-import Routes from './routes'
+import Routes from './Routes'
 
 ReactDOM
 .createRoot(document.getElementById('root')!)
@@ -11,7 +12,9 @@ ReactDOM
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
       <GlobalStyle/>
-      <Routes/>
+      <AuthProvider>
+        <Routes/>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
